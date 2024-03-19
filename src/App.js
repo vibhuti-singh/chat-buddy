acba5ed9d2a1a809bbe27301c9855d1e1c42e19c
root <root@root.com>
2024-03-19 16:10:04 +0530

    src new commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
-import socketIO from "socket.io-client";
-const serverURL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:4000" // Development server URL
-    : "https://8f38-113-193-214-154.ngrok-free.app"; // Production ngrok URL
-const socket = socketIO.connect(serverURL);
+import socketIO from "socket.io-client"
+
+const socket = socketIO.connect("http://localhost:4000")
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home socket={socket} />}></Route>
-          <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
-        </Routes>
-      </div>
+        <div>
+          <Routes>
+            <Route path="/" element={<Home socket={socket}/>}></Route>
+            <Route path="/chat" element={<ChatPage socket={socket}/>}></Route>
+          </Routes>
+    </div>
     </BrowserRouter>
+    
   );
 }
 
