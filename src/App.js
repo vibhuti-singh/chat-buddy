86f72cbd026b2514a959a3c9d6106197501d9b40
root <root@root.com>
2024-03-19 12:55:31 +0530

    src/App update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client";
-
-const socket = socketIO.connect("https://ten-dingos-laugh.loca.lt");
+const serverURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:4000" // Development server URL
+    : "https://8f38-113-193-214-154.ngrok-free.app"; // Production ngrok URL
+const socket = socketIO.connect(serverURL);
 function App() {
   return (
     <BrowserRouter>
